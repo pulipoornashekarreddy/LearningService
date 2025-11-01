c2f82cddbac649fc1a702324d2d29957c168df0b
ProoofAdmin <admin@prooof.in>
2025-11-01 15:10:36 +0530

    first commit

---
+++ src/main/java/com/perfectsde/LearningService/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.perfectsde.LearningService.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<Object> test() {
+        return new ResponseEntity<>("Learning Service is live", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/perfectsde/LearningService/LearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfectsde.LearningService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class LearningServiceApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
